6492f23a2be535935e354715c898cf3c9afddd9b
zuradel <53078525+zuradel@users.noreply.github.com>
2021-07-05 09:10:45 +0700

    test

---
+++ Model/DAO/PostDao.cs
@@ -0,0 +1,23 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+namespace Model.DAO
+{
+    
+    public class PostDao
+    {
+        PetStoreDbContext db = null;
+        public PostDao()
+        {
+            db = new PetStoreDbContext();
+        }
+        public IEnumerable<Post> listAllPaging(int page, int pageSize)
+        {
+            return db.Posts.OrderByDescending(x=>x.CreatedAt).ToPagedList(page, pageSize);
+        }
+    }
+}
---
+++ PetStore/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        PetStoreDbContext db = null;
+        // GET: Admin/Account
+        public ActionResult ListAccount()
+        {
+            db = new PetStoreDbContext();
+            List<User> listUser = db.Users.ToList();
+            return View(listUser);
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
