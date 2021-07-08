85f1687b222cb23e928bd11cf913128d352c7378
quangminhit1102 <quangminhit1102@gmail.com>
2021-07-08 17:57:35 +0700

    Minh 08/07/2021 17:57

--- PetStore/App_Start/RouteConfig.cs
+++ PetStore/App_Start/RouteConfig.cs
@@ -11,8 +11,7 @@ namespace PetStore
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");   
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ PetStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        PetStoreDbContext db = null;
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+    }
+}
