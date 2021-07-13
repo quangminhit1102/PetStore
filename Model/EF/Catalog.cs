0a8992b70484e0268de48c05563aef3687cb4349
zuradel <53078525+zuradel@users.noreply.github.com>
2021-07-14 03:31:34 +0700

    chức năng bài viết

--- Model/EF/Catalog.cs
+++ Model/EF/Catalog.cs
@@ -22,6 +22,8 @@ namespace Model.EF
 
         public bool? Status { get; set; }
 
+        public int? Total { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Post> Posts { get; set; }
     }
--- PetStore/Controllers/PostController.cs
+++ PetStore/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,42 @@ namespace PetStore.Controllers
 {
     public class PostController : Controller
     {
+        PetStoreDbContext db = null;
         // GET: Post
         public ActionResult Index()
         {
-            return View();
+            db = new PetStoreDbContext();
+            var listPost = db.Posts.ToList();
+            return View(listPost);
+         
+        }
+        public ActionResult Detail(int id)
+        {
+            db = new PetStoreDbContext();
+            var Post = db.Posts.Find(id);
+            var catelog = db.Catalogs.Where(x => x.Id == Post.CataId).FirstOrDefault();
+            ViewBag.catelog = catelog.Title;
+            return View(Post);
+        }
+        public ActionResult ListPostByCata(int id)
+        {
+            db = new PetStoreDbContext();
+            var listPost = db.Posts.Where(x => x.CataId == id).ToList();
+            var catelog = db.Catalogs.Find(id);
+            ViewBag.catelog = catelog.Title;
+            return View(listPost);
+        }
+        public ActionResult ListCatalog()
+        {
+            db = new PetStoreDbContext();
+
+            var listPost = db.Posts.ToList();
+            var listCatalog = db.Catalogs.Where(x=>x.Status == true).ToList();
+            foreach (var item in listCatalog)
+            {
+                item.Total = listPost.Where(x => x.CataId == item.Id).Count();
+            }
+            return View(listCatalog);
         }
     }
 }
