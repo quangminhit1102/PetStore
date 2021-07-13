c08d4610cc8fe4a84bab405a4ec4b1fc05854eba
quangminhit1102 <quangminhit1102@gmail.com>
2021-07-13 17:44:51 +0700

    Alo

--- PetStore/Controllers/HomeController.cs
+++ PetStore/Controllers/HomeController.cs
@@ -1,18 +1,33 @@
 ﻿using Model.EF;
+using PetStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace PetStore.Controllers
 {
-    
+
     public class HomeController : Controller
     {
+        PetStoreDbContext db = null;
         public ActionResult Index()
         {
-            return View();
+            db = new PetStoreDbContext();
+            var listProduct = db.Products.Take(32).ToList();
+            HomeModel homemodel = new HomeModel();
+            homemodel.listProduct = listProduct;
+            return View(homemodel);
+        }
+
+        public ActionResult LoadMenu()
+        {
+            db = new PetStoreDbContext();
+            var listCategory = db.Categories.ToList();
+            
+            return PartialView(listCategory);
         }
     }
 }
